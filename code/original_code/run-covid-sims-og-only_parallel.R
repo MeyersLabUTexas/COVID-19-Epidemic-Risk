0396fa405dbcfcf6b9e4d4474d5a417261a6cb38
Emily Javan <emjavan@gmail.com>
2022-11-17 18:26:19 -0600

    update missing params

--- code/original_code/run-covid-sims-og-only_parallel.R
+++ code/original_code/run-covid-sims-og-only_parallel.R
@@ -16,13 +16,14 @@ source("original_code/sim-covid-outbreaks.R")
 print("og sim file found/added")
 
 ## Setup all of the parameters that need to be run
-args             = commandArgs(TRUE)
-r_not            = as.double(args[1]) # R0 = 1.1, 1.5, 3
-num_runs         = 100000
+args                  = commandArgs(TRUE)
+r_not                 = as.double(args[1]) # R0 = 1.1, 1.5, 3
 
 ## Run and save simulations across all parameter combinations
 output_path = "../processed_data/original_params_test"
 if(!dir.exists(output_path)){
   dir.create(output_path)
 }
-save_covid_runs(r_not=r_not, num_reps = num_runs, refresh=TRUE, dir_path=output_path)
+
+save_covid_runs(r_not=r_not, detection_probability=0.1, importation_rate=0, 
+  num_reps=100000, refresh=TRUE, dir_path=output_path)
