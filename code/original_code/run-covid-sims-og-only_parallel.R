1fb48b4e00d4fafcce023325796e8b6e0d05a1bb
Emily Javan <emjavan@gmail.com>
2022-11-17 18:06:00 -0600

    gen_time doesn't vary in original code

--- code/original_code/run-covid-sims-og-only_parallel.R
+++ code/original_code/run-covid-sims-og-only_parallel.R
@@ -18,8 +18,6 @@ print("og sim file found/added")
 ## Setup all of the parameters that need to be run
 args             = commandArgs(TRUE)
 r_not            = as.double(args[1]) # R0 = 1.1, 1.5, 3
-gen_interval     = as.double(args[2]) # gen_time = 5, 5.5, 6
-run_df           = expand_grid(r_not, gen_interval)
 num_runs         = 100000
 
 ## Run and save simulations across all parameter combinations
@@ -27,6 +25,6 @@ output_path = "../processed_data/original_params_test"
 if(!dir.exists(output_path)){
   dir.create(output_path)
 }
-run_df %>% # pipe the 4 inputs into save_covid_runs function, when refresh is FALSE it will not overwrite existing output
+r_not %>% # pipe the 4 inputs into save_covid_runs function, when refresh is FALSE it will not overwrite existing output
   pmap(.f = save_covid_runs, num_reps = num_runs, refresh=TRUE, dir_path=output_path) %>% 
   unlist()
